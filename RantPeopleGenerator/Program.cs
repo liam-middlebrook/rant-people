d045104ce9cd7de8842f34350f7544d4441b2e7b
Liam Middlebrook <liammiddlebrook@gmail.com>
2015-02-20 18:42:13 -0500

    file output

--- RantPeopleGenerator/Program.cs
+++ RantPeopleGenerator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,7 @@ namespace RantPeopleGenerator
             RantEngine engine = new RantEngine(Path.Combine(Directory.GetCurrentDirectory(), "vocab"));
             string yamlTemplate = "";
             StreamReader sr = null;
+
             try
             {
                 sr = new StreamReader(Path.Combine(Directory.GetCurrentDirectory(), "template.yaml"));
@@ -31,12 +33,50 @@ namespace RantPeopleGenerator
                     sr.Close();
                 }
             }
-            for (int i = 0; i < 3; i++)
+
+            if (!Directory.Exists(Path.Combine(Directory.GetCurrentDirectory(), "output")))
             {
-                Console.WriteLine(engine.Do(yamlTemplate)+"\n\n\n");
+                Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "output"));
             }
 
-            Console.ReadLine();
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            for (int i = 0; i < 32767; i++)
+            {
+                string firstName = engine.Do("<name>");
+                string lastName = engine.Do("<noun><noun>");
+                string nick = engine.Do("<adj><noun-animal>");
+                string tld = engine.Do("<noun>");
+                string noun0 = engine.Do("<noun>");
+                string noun1 = engine.Do("<noun.plural>");
+                string verb0 = engine.Do("<verb>");
+                string num0 = engine.Do("[n:1990;2035]");
+                string noun2 = engine.Do("<noun-animal>");
+                string num1 = engine.Do("[n:10;99]");
+                string adj0 = engine.Do("<adj>");
+                string noun3 = engine.Do("<noun-animal>");
+                string verb1 = engine.Do("<verb>");
+                File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), "output", nick.Replace(" ", "-") + ".yaml"),
+                    string.Format(
+                    yamlTemplate,
+                    firstName.Replace(" ","-"),
+                    lastName.Replace(" ", "-"),
+                    nick.Replace(" ", "-"),
+                    tld.Replace(" ", "-"),
+                    noun0.Replace(" ", "-"),
+                    noun1.Replace(" ", "-"),
+                    verb0.Replace(" ", "-"),
+                    num0.Replace(" ", "-"),
+                    noun2.Replace(" ", "-"),
+                    num1.Replace(" ", "-"),
+                    adj0.Replace(" ", "-"),
+                    noun3.Replace(" ", "-"),
+                    verb1.Replace(" ", "-")));
+            }
+            watch.Stop();
+
+            Console.WriteLine("Generation took {0} seconds!", watch.Elapsed.TotalSeconds);
+            Console.Read();
         }
     }
 }
